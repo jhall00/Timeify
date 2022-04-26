493d661b69166125c7545adef238261ff2faab85
Noah Norton <noahnorton1207@gmail.com>
2022-04-25 20:29:54 -0400

    Added validation for a zero-length playlist. Cleaned up gen_validate.js as well.

--- public/gen_validate.js
+++ public/gen_validate.js
@@ -1,56 +1,68 @@
-var hours_input = document.querySelector('select[name="length_hours"]');
-var minutes_input = document.querySelector('select[name="length_minutes"]');
-var seconds_input = document.querySelector('select[name="length_seconds"]');
-var name_input = document.querySelector('input[name="newName"]');
+var hours = document.querySelector('select[name="length_hours"]');
+var minutes = document.querySelector('select[name="length_minutes"]');
+var seconds = document.querySelector('select[name="length_seconds"]');
+var hoursVal;
+var minsVal;
+var secsVal;
+var timerName = document.querySelector('input[name="newName"]');
 var generate_form = document.getElementById("generate_form");
 
 
-hours_input.addEventListener('invalid',  (event) => {
-    if (event.target.validity.valueMissing) {
-        event.target.setCustomValidity('Please select the number of hour(s) for your timer.');
+
+hours.addEventListener('invalid',  (e) => {
+    if (e.target.validity.valueMissing) {
+        e.target.setCustomValidity('Please select the number of hour(s) for your timer.');
     }
 })
 
-minutes_input.addEventListener('invalid', (event) => {
-    if (event.target.validity.valueMissing) {
-        event.target.setCustomValidity('Please select the number of minute(s) for your timer.');
+minutes.addEventListener('invalid', (e) => {
+    if (e.target.validity.valueMissing) {
+        e.target.setCustomValidity('Please select the number of minute(s) for your timer.');
     }
 })
 
-seconds_input.addEventListener('invalid', (event) => {
-    if (event.target.validity.valueMissing) {
-        event.target.setCustomValidity('Please select the number of second(s) for your timer.');
+seconds.addEventListener('invalid', (e) => {
+    if (e.target.validity.valueMissing) {
+        e.target.setCustomValidity('Please select the number of second(s) for your timer.');
     }
 })
 
-name_input.addEventListener('invalid', (event) => {
-    if (event.target.validity.valueMissing) {
-        event.target.setCustomValidity('Please enter a name for your timer.');
+timerName.addEventListener('invalid', (e) => {
+    if (e.target.validity.valueMissing) {
+        e.target.setCustomValidity('Please enter a name for your timer.');
     }
 })
 
 
-hours_input.addEventListener('change', (event) => {
-    event.target.setCustomValidity('');
+hours.addEventListener('change', (e) => {
+    e.target.setCustomValidity('');
+    hoursVal = hours.options[hours.selectedIndex].value;
 })
 
-minutes_input.addEventListener('change', (event) => {
-    event.target.setCustomValidity('');
+minutes.addEventListener('change', (e) => {
+    e.target.setCustomValidity('');
+    minsVal = minutes.options[minutes.selectedIndex].value;
 })
 
-seconds_input.addEventListener('change', (event) => {
-    event.target.setCustomValidity('');
+seconds.addEventListener('change', (e) => {
+    e.target.setCustomValidity('');
+    secsVal = seconds.options[seconds.selectedIndex].value;
 })
 
-name_input.addEventListener('change', (event) => {
-    event.target.setCustomValidity('');
+timerName.addEventListener('change', (e) => {
+    e.target.setCustomValidity('');
 })
 
 
-generate_form.addEventListener('submit', (event) => {
+generate_form.addEventListener('submit', (e) => {
     if (document.getElementById('selected').innerHTML == ''){
-        event.preventDefault();
-        alert("Please select a playlist source using the Select Source button.")
+        e.preventDefault();
+        alert("Please select a playlist source using the Select Source button.");
         console.log("No source selected.");
+    }
+    if(hoursVal == 0 && minsVal == 0 && secsVal == 0){
+        e.preventDefault();
+        alert("Please specify a timer length that is above zero hours, zero minutes, and zero seconds.");
+        console.log("Length of timer is 0:00:00.");
     }
 })
